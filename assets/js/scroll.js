1eb662c161734cae81fe7138a4a9fac92011bdab
Jolene <jolene.lim14@gmail.com>
2020-03-15 01:04:25 -0400

    Change stylesheet using sass

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -11,3 +11,27 @@ function scrollFeatures() {
   document.getElementById('landing').scrollTop = topPos-10;
 
 };
+
+$(document).ready(function() {
+  $('section#landing').on('scroll', function() {
+
+    var aboutPos = document.getElementById('about').offsetTop;
+    var featurePos = document.getElementById('features').offsetTop;
+    
+    if ($('#landing').scrollTop() < featurePos) {
+      $('.dropdown-menu').removeClass('features');
+      $('.dropdown-menu').removeClass('about');
+    }
+
+    else if ($('#landing').scrollTop() < aboutPos) {
+      $('.dropdown-menu').toggleClass('features');
+    }
+
+    else if ($('#landing').scrollTop() >= aboutPos) {
+      $('.dropdown-menu').toggleClass('about');
+    }
+
+    /*
+    $('btn').toggleClass('scrolled', $('#landing').scrollTop() > topPos);*/
+  });
+});
